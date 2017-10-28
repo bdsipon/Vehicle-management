e2f4a4ff8cc144acd37a60ec555fc55754de895e
bdsipon <sipon.here@gmail.com>
2017-10-29 03:03:17 +1000

    dropdown updated

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleFormComponent implements OnInit {
   makes:any;
+  vehicle={};
 
   constructor(private makeService:MakeService) { }
 
@@ -19,6 +20,10 @@ export class VehicleFormComponent implements OnInit {
     }
   );
 
+  }
+  onMakeChange(){
+    console.log("VEHICLE",this.vehicle);
+    
   }
 
 }
