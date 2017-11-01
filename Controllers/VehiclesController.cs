01ec9160dd79e976b80aad8e2f81653a1b7da238
bdsipon <sipon.here@gmail.com>
2017-11-02 02:05:14 +1000

    commit

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Vehicle_management.Controllers.Resources;
 using Vehicle_management.models;
 
 namespace Vehicle_management.Controllers
@@ -6,9 +8,19 @@ namespace Vehicle_management.Controllers
     [Route("/api/vehicles")]
     public class VehiclesController:Controller
     {
-        [HttpPost]
-        public IActionResult CreateVehicle(Vehicle vehicle)
+        private readonly IMapper mapper;
+        public  VehiclesController (IMapper  mapper)  
         {
+            this.mapper=mapper;
+           
+        }
+
+        public IMapper Mapper { get; }
+
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle=mapper.Map<VehicleResource,Vehicle>(vehicleResource);
             return Ok(vehicle);
 
         }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Vehicle_management.Controllers.Resources;
 using Vehicle_management.models;
+using System.Linq;
 
 namespace Vehicle_management.Mapping
 {
@@ -8,9 +9,17 @@ namespace Vehicle_management.Mapping
     {
         public MappingProfile()
         {
+            //Domain to Api Resource
             CreateMap<Make, MakeResource>();
             CreateMap<Model,ModelResouce>();
             CreateMap<Feature,FeatureResource>();
+            
+
+            CreateMap<VehicleResource,Vehicle>()
+            .ForMember(v => v.ContactName,opt=>opt.MapFrom(vr =>vr.Contact.Name))
+             .ForMember(v => v.ContactEmail,opt=>opt.MapFrom(vr =>vr.Contact.Email))
+              .ForMember(v => v.ContactPhone,opt=>opt.MapFrom(vr =>vr.Contact.Phone))
+               .ForMember(v => v.Features,opt=>opt.MapFrom(vr =>vr.Features.Select(id=>new VehicleFeature{FeatureId=id})));
         }
     }
 }
